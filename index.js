723f3962f1c67c41e7c09717d76597084f1f44fd
Kim Love <kimberlymlove15@gmail.com>
2023-08-02 19:49:29 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arrayOfNames, eventName){
+    let newArray = [];
+
+    for(let i = 0; i < arrayOfNames.length; i++){
+        let message = `Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`;
+        newArray.push(message);
+    }
+
+    return newArray;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
